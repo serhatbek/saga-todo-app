213537bfd56e1dc9f7da22ac580f97e52e343dfa
serhatbek <serhat.bekk@gmail.com>
2023-11-27 17:07:57 +0300

    fix input clear when focus out

--- src/components/AddTodoItem/AddTodoItem.jsx
+++ src/components/AddTodoItem/AddTodoItem.jsx
@@ -2,11 +2,30 @@ import Button from '../Button/Button';
 import { IoMdAddCircle } from 'react-icons/io';
 import Input from '../Input/Input';
 import './AddTodoItem.scss';
+import { useState } from 'react';
 
 const AddTodoItem = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  // Event handler to handle input change
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // Event handler to clear input on blur
+  const handleInputBlur = () => {
+    setInputValue(''); // Clear input value on blur
+  };
+
   return (
     <div className='add-todo'>
-      <Input className='input' placeholder='Add todo' />
+      <Input
+        value={inputValue}
+        onChange={handleInputChange}
+        onBlur={handleInputBlur}
+        className='input'
+        placeholder='Add todo'
+      />
       <Button iconLeft={<IoMdAddCircle />}>ADD</Button>
     </div>
   );
